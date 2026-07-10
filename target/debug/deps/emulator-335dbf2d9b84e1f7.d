ded47890edc41ab8499cce8a9123cd4d2ec21e1c
Arjun Manikandan <axm5507@gmail.com>
2026-07-10 12:51:31 -0500

    Add .gitignore, stop tracking build artifacts

--- target/debug/deps/emulator-335dbf2d9b84e1f7.d
+++
@@ -1,7 +0,0 @@
-C:\Users\axm55\OneDrive\gameboy-emulator\emulator\target\debug\deps\emulator-335dbf2d9b84e1f7.d: src\main.rs src\cpu\mod.rs src\cpu\registers.rs
-
-C:\Users\axm55\OneDrive\gameboy-emulator\emulator\target\debug\deps\emulator-335dbf2d9b84e1f7.exe: src\main.rs src\cpu\mod.rs src\cpu\registers.rs
-
-src\main.rs:
-src\cpu\mod.rs:
-src\cpu\registers.rs:
