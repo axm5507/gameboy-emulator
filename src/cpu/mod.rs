b7ddfbdb2d8cd7a862966f9c2865a02daa4e5283
Arjun Manikandan <146555946+axm5507@users.noreply.github.com>
2026-07-10 21:49:54 -0500

    Update mod.rs with new files

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,3 +1,7 @@
 pub mod registers;
+pub mod instruction;
+mod cpu;
 
+pub use cpu::CPU;
+pub use instruction::{ADDHLTarget, ArithmeticTarget, BitPosition, Instruction};
 pub use registers::Registers;
